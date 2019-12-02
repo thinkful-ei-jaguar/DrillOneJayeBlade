23eaf430dd59344c3382fbf4c18c345912a8eed3
Blade Boles <bladerboles@gmail.com>
2019-12-02 14:07:27 -0700

    added yearOfBirth

--- DrillOne.js
+++ DrillOne.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
 function createGreeting(name, age) {
-  return `Hi, my name is ${name}.`;
+  let yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name}.  I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting('Blade');
