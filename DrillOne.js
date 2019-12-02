19bea7b639c9bd52a185971b7c8d560e3529c44e
Blade Boles <bladerboles@gmail.com>
2019-12-02 14:05:01 -0700

    added name and age paramters

--- DrillOne.js
+++ DrillOne.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
-function createGreeting() {
-  return 'Hi, my name is Jaye';
+function createGreeting(name, age) {
+  return `Hi, my name is ${name}.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Blade');
 console.log(greeting1);
